1b6854c4c67e64c9e3d06def03042f376a8f67af
jemsotry <jemsotry@gmail.com>
2016-05-11 22:17:11 -0500

    Launching our first BETA releas2e

--- server.js
+++ server.js
@@ -6,18 +6,10 @@
 var app = require('./config/lib/app');
 var server = app.start();
 
+
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-//server.listen(server_port, server_ip_address, function(){
-  //console.log("Listening on " + server_ip_address + ", server_port " + server_port);});
-
-var mean = require('meanio');
-
-// Creates and serves mean application
-mean.serve({ /*options placeholder*/ }, function(app, config) {
-  console.log('Mean app started on port ' + config.http.port + ' (' + process.env.NODE_ENV + ')');
-  if(config.https && config.https.port){
-    console.log('Mean secure app started on port ' + config.https.port + ' (' + process.env.NODE_ENV + ')');
-  }
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port);
 });
